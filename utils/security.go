f9de05663a9bf83fd695d1ecac2b2c129dc2878e
buptczq <czq@qq.com>
2020-11-20 15:16:26 +0800

    fix: rewrite security functions

--- utils/security.go
+++ utils/security.go
@@ -3,58 +3,10 @@ package utils
 import (
 	"golang.org/x/sys/windows"
 	"syscall"
-	"unsafe"
 )
-
-var (
-	modadvapi32         = windows.NewLazySystemDLL("advapi32.dll")
-	procGetSecurityInfo = modadvapi32.NewProc("GetSecurityInfo")
-)
-
-const (
-	seKernelObject           = 6
-	ownerSecurityInformation = 1
-)
-
-func getSecurityInfo(
-	handle windows.Handle,
-	objectType uint32,
-	securityInformation uint32,
-	ppsidOwner **windows.SID,
-	ppsidGroup **windows.SID,
-	ppDacl uintptr,
-	ppSacl uintptr,
-	ppSecurityDescriptor *windows.Handle,
-) (err error) {
-	r1, _, e1 := syscall.Syscall9(
-		procGetSecurityInfo.Addr(),
-		8,
-		uintptr(handle),
-		uintptr(objectType),
-		uintptr(securityInformation),
-		uintptr(unsafe.Pointer(ppsidOwner)),
-		uintptr(unsafe.Pointer(ppsidGroup)),
-		uintptr(unsafe.Pointer(ppDacl)),
-		uintptr(unsafe.Pointer(ppSacl)),
-		uintptr(unsafe.Pointer(ppSecurityDescriptor)),
-		0,
-	)
-	if r1 != 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
 
 func GetUserSID() (*windows.SID, error) {
-	token, err := windows.OpenCurrentProcessToken()
-	if err != nil {
-		return nil, err
-	}
-	defer token.Close()
+	token := windows.GetCurrentProcessToken()
 	user, err := token.GetTokenUser()
 	if err != nil {
 		return nil, err
@@ -63,14 +15,11 @@ func GetUserSID() (*windows.SID, error) {
 }
 
 func GetHandleSID(h windows.Handle) (*windows.SID, error) {
-	var sid, gid *windows.SID
-	var psd windows.Handle
-	err := getSecurityInfo(h, seKernelObject, ownerSecurityInformation, &sid, &gid, 0, 0, &psd)
-	defer func() {
-		if psd != 0 {
-			windows.LocalFree(psd)
-		}
-	}()
+	sd, err := windows.GetSecurityInfo(h, windows.SE_KERNEL_OBJECT, windows.OWNER_SECURITY_INFORMATION)
+	if err != nil {
+		return nil, err
+	}
+	sid, _, err := sd.Owner()
 	if err != nil {
 		return nil, err
 	}
@@ -78,11 +27,7 @@ func GetHandleSID(h windows.Handle) (*windows.SID, error) {
 }
 
 func GetDefaultSID() (*windows.SID, error) {
-	proc, err := windows.GetCurrentProcess()
-	if err != nil {
-		return nil, err
-	}
-	defer windows.CloseHandle(proc)
+	proc := windows.CurrentProcess()
 	return GetHandleSID(proc)
 }
 
