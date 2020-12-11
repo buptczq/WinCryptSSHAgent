buptczq/WinCryptSSHAgent

commit 73810bdd3ed59f093aab39696c28a4b869330e5c

fix: change default sockfile path

app/wsl.go
@@ -19,10 +19,11 @@ type WSL struct {
 }
 
 func listenUnixSock(filename string) (string, net.Listener, error) {
-	path, err := filepath.Abs(filename)
+	home, err := os.UserHomeDir()
 	if err != nil {
 		return "", nil, err
 	}
+	path := filepath.Join(home, CYGWIN_SOCK)
 	os.Remove(path)
 	l, err := net.Listen("unix", path)
 	return path, l, err

app/cygwin.go
@@ -66,10 +66,11 @@ func cygwinHandshake(conn net.Conn, uuid []byte) error {
 }
 
 func (s *Cygwin) Run(ctx context.Context, handler func(conn io.ReadWriteCloser)) error {
-	sockfile, err := filepath.Abs(CYGWIN_SOCK)
+	home, err := os.UserHomeDir()
 	if err != nil {
 		return err
 	}
+	sockfile := filepath.Join(home, CYGWIN_SOCK)
 	s.sockfile = sockfile
 	// listen tcp socket
 	l, err := net.Listen("tcp", "localhost:0")

sshagent/certificate.go
@@ -6,10 +6,16 @@ import (
 	"golang.org/x/crypto/ssh"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func loadCertFile(filename string) (*ssh.Certificate, error) {
-	file, err := os.Open(filename)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+	path := filepath.Join(home, filename)
+	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}

commit bc7ed94d4b2bd2a762ecea659228bdedacd3d40d

fix: correct WSL socket file name
it got broken in 73810bdd

app/wsl.go
@@ -23,7 +23,7 @@ func listenUnixSock(filename string) (string, net.Listener, error) {
 	if err != nil {
 		return "", nil, err
 	}
-	path := filepath.Join(home, CYGWIN_SOCK)
+	path := filepath.Join(home, WSL_SOCK)
 	os.Remove(path)
 	l, err := net.Listen("unix", path)
 	return path, l, err