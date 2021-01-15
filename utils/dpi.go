fecacdf24420536c255c42c27ffcfc123c1770d3
buptczq <czq@qq.com>
2021-01-15 23:31:04 +0800

    fix: DPI compatibility

--- utils/dpi.go
+++ utils/dpi.go
@@ -8,9 +8,28 @@ var (
 )
 
 func SetProcessSystemDpiAware() error {
-	if err := setProcessDpiAwareness.Find(); err != nil {
+	if setProcessDpiAwarenessContext, err := user32.FindProc("SetProcessDpiAwarenessContext"); err == nil {
+		// DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2
+		var DPI_AWARENESS_CONTEXT uintptr = 0
+		r0, _, _ := setProcessDpiAwarenessContext.Call(DPI_AWARENESS_CONTEXT - 4)
+		if r0 == 1 {
+			return nil
+		}
+	}
+	if err := setProcessDpiAwareness.Find(); err == nil {
+		// PROCESS_SYSTEM_DPI_AWARE
+		r0, _, err := setProcessDpiAwareness.Call(uintptr(1))
+		if r0 == 1 {
+			return nil
+		}
 		return err
 	}
-	_, _, err := setProcessDpiAwareness.Call(uintptr(1))
-	return err
+	if setProcessDpiAware, err := user32.FindProc("SetProcessDPIAware"); err == nil {
+		r0, _, err := setProcessDpiAware.Call()
+		if r0 == 1 {
+			return nil
+		}
+		return err
+	}
+	return nil
 }
