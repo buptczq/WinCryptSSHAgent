4214b1477d69139f1604b0a03b01ee969a1815ec
CzBiX <gliuwr@gmail.com>
2021-02-21 20:35:35 +0800

    Refine build script

--- build.bat
+++ build.bat
@@ -1,6 +1,24 @@
-set GOARCH=amd64
-go generate
-go build -ldflags -H=windowsgui
-set GOARCH=386
-go build -ldflags -H=windowsgui -o WinCryptSSHAgent_32bit.exe
+@echo off
+setlocal
 
+@rem Build all by default
+if [%1]==[] (
+	go generate
+	call :build 386 WinCryptSSHAgent_32bit.exe
+	call :build amd64 WinCryptSSHAgent.exe
+) else (
+	go generate
+	call :build %1 WinCryptSSHAgent-%1.exe
+)
+goto :eof
+
+
+:build
+set arch=%1
+set output=%2
+echo Build %arch% to %output%
+
+set GOARCH=%arch%
+go build -ldflags "-w -s -H=windowsgui" -trimpath -o %output%
+
+goto :eof
